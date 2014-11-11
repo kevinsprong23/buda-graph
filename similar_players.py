db4b387395e897a4593ca022428281f58c894cc7
Kevin Sprong <kevinsprong@gmail.com>
2014-11-11 17:20:03 -0500

    print for similarity status

--- similar_players.py
+++ similar_players.py
@@ -41,6 +41,30 @@ def build_adjacency_vector(nodes, edges, player_id):
     return adjacency_vec
 
 
+def build_adjacency_matrix(nodes, edges):
+    """
+    build the full adjacency matrix for the undirected graph
+    defined by the edge list in edges
+    
+    nodes is the node list, and is only used for matrix sizing
+    edges has tuple of integer keys for source/target, and weight values
+    """
+    num_nodes = len(nodes)
+
+    # init the matrix - this would be easier with numpy
+    adj_mat = [0 for n in range(num_nodes)]
+    for i in range(num_nodes):
+        adj_mat[i] = [0 for n in range(num_nodes)]
+
+    # loop over edges and assign to matrix
+    for key, weight in edges.items():
+        src, tgt = key
+        adj_mat[src-1][tgt-1] = weight
+        adj_mat[tgt-1][src-1] = weight
+        
+    return adj_mat
+
+
 def jaccard_similarity(vector_one, vector_two):
     """
     weighted jaccard similarity between two vectors:
@@ -71,32 +95,45 @@ def cosine_similarity(vector_one, vector_two):
     return dot(vector_one, vector_two) / (vec_one_norm * vec_two_norm)
 
 
-def find_missing_edges(nodes, edges, num_to_find=20, node_thresh=1):
+def find_missing_edges(adj_mat, player_id=None, num_to_find=50, node_thresh=1):
     """
     find the most similar players that haven't played together
 
-    nodes is a dictionary of id : name
-    edges is a dictionary of source,target : weight by node id
+    adj_mat is the adjacency matrix, element ij is the weight between
+    nodes with id i+1 and j+1 (because matrices are zero-indexed)
+
+    player_id is a specific player id to limit the recommendations to
     num_to_find is the number of missing edges to track
     node_thresh filters on node weighted degree - dont search small nodes
+
+    returns NODE IDs (not matrix indices) and similarity scores
     """
-    N = len(nodes)
     # tuples of uid1,uid2,similarity
     missing_edges = [(-1, -2, float('-inf')) for n in range(num_to_find)]
 
+    N = len(nodes)
+    if player_id:
+        ids_to_search = range(player_id, player_id+1)
+    else:
+        ids_to_search = range(1, N+1)
+
     # player uids range from 1 to N
-    for uid in range(1, N+1):  # to second-to-last index
-        player_one_vec = build_adjacency_vector(nodes, edges, uid)
+    for uid in ids_to_search:  # to second-to-last index
+        print("Searching player {0}".format(uid), flush=True)
+
+        player_one_vec = adj_mat[uid - 1] 
 
         if sum(player_one_vec) <= node_thresh:
             continue
 
-        for uid2 in range(uid+1, N+1):  # to last index
+        ids_to_compare = range(1, N+1) if player_id else range(uid+1, N+1)
+
+        for uid2 in ids_to_compare:  # to last index
             # only want unconnected players
-            if (uid, uid2) in edges or (uid2, uid) in edges:
+            if adj_mat[uid - 1][uid2 - 1] or uid == uid2:
                 continue
 
-            player_two_vec = build_adjacency_vector(nodes, edges, uid2)
+            player_two_vec = adj_mat[uid2 - 1]
 
             if sum(player_two_vec) <= node_thresh:
                 continue
@@ -129,11 +166,11 @@ if __name__ == "__main__":
             src, tgt, lbl, wgt, typ = line.strip().split(',')
             edges[(int(src), int(tgt))] = int(wgt)
 
-    similar_players = find_missing_edges(nodes, edges)
+    adj_mat = build_adjacency_matrix(nodes, edges)
+    similar_players = find_missing_edges(adj_mat, node_thresh=50)
 
     for uid, uid2, sim in similar_players:
         print(nodes[uid], nodes[uid2], sim, sep=', ')
-
 
 
 
