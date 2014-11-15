1d97f3c13a8da08cdebde75c3fe0ecdec3d26d39
Kevin Sprong <kevinsprong@gmail.com>
2014-11-15 16:28:20 -0500

    streamlined output by storing ints in similarity results

--- tmp/jsonify_results.py
+++ tmp/jsonify_results.py
@@ -14,12 +14,25 @@ if __name__ == "__main__":
                 similarity_results[player] = []
             similarity_results[player].append((player2, float(sim)))
 
+    # nodes
+    nodes = {}
+    with open('../data/player_graph/nodes.csv') as file_in:
+        next(file_in)  # skip header row
+        for line in file_in:
+            player_id, label = line.strip().split(',')
+            nodes[label] = int(player_id)
+
     # jsonify
-    jsonified = []
+    jsonified_results = []
+    jsonified_nodes = []
     for player in similarity_results:
-        j = [{'n': n, 's': s} for n, s in similarity_results[player]]
-        jsonified.append({'name': player, 'players': j})
+        j = [{'n': nodes[n], 's': s} for n, s in similarity_results[player]]
+        pid = nodes[player]
+        jsonified_results.append({'id': pid, 'list': j})
+        jsonified_nodes.append({'id': pid, 'label': player})
     
     # save
-    with open('similarities.json', 'w') as file_out:
-        print(json.dumps(jsonified), file=file_out)
+    with open('similarities.json', 'w') as file_out, \
+         open('nodes.json', 'w') as node_file_out:
+        print(json.dumps(jsonified_results), file=file_out)
+        print(json.dumps(jsonified_nodes), file=node_file_out)
